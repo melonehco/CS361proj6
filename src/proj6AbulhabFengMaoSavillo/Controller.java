4bc9f1c0776286afedf8d4e717339ce2d129334f
Yi Feng <yifeng20@colby.edu>
2018-10-26 22:16:04 -0400

    changed comment

--- src/proj6AbulhabFengMaoSavillo/Controller.java
+++ src/proj6AbulhabFengMaoSavillo/Controller.java
@@ -416,7 +416,7 @@ public class Controller
     }
 
     /**
-     * Calls the method that handles the tree item clicked action from the structureViewController.
+     * Jump to the line where the selected class/method/field is declared.
      *
      * @param event
      */
